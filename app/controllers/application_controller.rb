683c561aaf141e323810cfebadf64652ae32d005
kaitotenjo <48242099+kaitotenjo@users.noreply.github.com>
2019-03-26 11:46:48 +0700

    new update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-	class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	include SessionsHelper
 	
@@ -6,4 +6,19 @@ end
 class UsersController <  ActionController::Base
 end
 class MicropostsController < ApplicationController
+end
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
+  end
+
+  def destroy
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
+  def setup
+    @user = users(:michael)
+  end
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -10,5 +12,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal FILL_IN, assigns(:user).FILL_IN
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 end
